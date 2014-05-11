bbc7195844bb2b4a67d7c3c2d84467bfaae85363
Chelsea Winfree <chelsea.winfree@rackspace.com>
2014-05-11 04:02:15 -0500

    pep8 tweaks and better organization

--- fibonacci.py
+++ fibonacci.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 class SleepyFib:
+    """
+    Contains the calculation and validation for app
+    """
     errors = {
         "NotIntegerError": "Only positive integers, please",
         "Over9000Error": "Your value is over 9000, please try again"
         }
 
     def fib(self, n):
+        # Returns a list from the result of calc_fib generator
+
         return list(self.calc_fib(n))
 
     def calc_fib(self, n):
+        # Fibonacci sequence generator
         a, b = 0, 1
         for _ in xrange(n):
             yield a
             a, b = b, a + b
 
     def validate_fib(self, n):
-        # Validates the data before it gets passed into function
+        # Validates data before calc_fib generator is called.
         if isinstance(n, int) is False:
             response = self.errors["NotIntegerError"]
         elif n < 1:
--- tests/test_fib.py
+++ tests/test_fib.py
@@ -5,6 +5,9 @@ from fibonacci import SleepyFib
 
 
 class TestFib:
+    """
+    Tests performed against fibonnacci code
+    """
 
     def test_fib_one(self):
         # Verify initial value hasn't changed
@@ -16,7 +19,15 @@ class TestFib:
         result = SleepyFib().fib(40)
         assert result[-1] == 63245986
 
-    def test_fib_negative_number(self):
+
+class TestAPI:
+    """
+    Tests performed against SleepyFib API.
+
+    Uses requests directly and compares json to expected outcomes.
+    """
+
+    def test_fib_negative(self):
         # Verify negative numbers aren't allowed
         result = requests.get("http://localhost:5000/sleepyfib/api/fib/-5")
         message = SleepyFib().errors["NotIntegerError"]
